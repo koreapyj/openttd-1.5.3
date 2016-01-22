b595b068d026b690a817c2df003cf759594cc67d
Yoonji Park <Yoonji Park>
2016-01-22 18:21:11 +0900

    Setting is now saved on company settings table.

--- src/saveload/company_sl.cpp
+++ src/saveload/company_sl.cpp
@@ -313,6 +313,10 @@ static const SaveLoad _company_settings_desc[] = {
 	SLE_CONDVAR(Company, settings.vehicle.servint_aircraft,  SLE_UINT16,     120, SL_MAX_VERSION),
 	SLE_CONDVAR(Company, settings.vehicle.servint_ships,     SLE_UINT16,     120, SL_MAX_VERSION),
 
+	/* Infrastructure sharing settings */
+	SLE_CONDARR(Company, settings.infrastructure_sharing, SLE_BOOL, 4, 200, SL_MAX_VERSION),
+	SLE_CONDARR(Company, settings.sharing_fee,            SLE_UINT32, 4, 200, SL_MAX_VERSION),
+
 	SLE_CONDNULL(63, 2, 143), // old reserved space
 
 	SLE_END()
@@ -334,6 +338,10 @@ static const SaveLoad _company_settings_skip_desc[] = {
 	SLE_CONDNULL(2, 120, SL_MAX_VERSION),    // settings.vehicle.servint_roadveh
 	SLE_CONDNULL(2, 120, SL_MAX_VERSION),    // settings.vehicle.servint_aircraft
 	SLE_CONDNULL(2, 120, SL_MAX_VERSION),    // settings.vehicle.servint_ships
+
+	/* Infrastructure sharing settings */
+	SLE_CONDNULL(4, 200, SL_MAX_VERSION),    // settings.infrastructure_sharing
+	SLE_CONDNULL(16, 200, SL_MAX_VERSION),    // settings.sharing_fee
 
 	SLE_CONDNULL(63, 2, 143), // old reserved space
 
